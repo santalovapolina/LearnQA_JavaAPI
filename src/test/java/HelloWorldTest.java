544461015cb9734d7f9d91cef06e4e61cf6a2523
Polina Santalova <polina.santalova@ecm-consulting.ru>
2025-02-05 19:17:59 +0300

    first commit

---
+++ src/test/java/HelloWorldTest.java
@@ -0,0 +1,11 @@
+import org.junit.jupiter.api.Test;
+
+import java.sql.SQLOutput;
+
+public class HelloWorldTest {
+
+    @Test
+    public void testHelloWorld(){
+        System.out.println("Hello world");
+    }
+}
