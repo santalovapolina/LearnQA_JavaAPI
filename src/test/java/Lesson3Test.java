9c8913098e628afab1fef8e7d18344de9b611679
Polina Santalova <polina.santalova@ecm-consulting.ru>
2025-02-21 19:17:05 +0300

    add testActualHeader()

--- src/test/java/Lesson3Test.java
+++ src/test/java/Lesson3Test.java
@@ -1,11 +1,12 @@
+import io.restassured.http.Header;
+import io.restassured.http.Headers;
 import io.restassured.response.Response;
 import org.junit.jupiter.api.Test;
 
 import java.util.Map;
 
 import static io.restassured.RestAssured.given;
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class Lesson3Test {
 
@@ -15,11 +16,6 @@ public class Lesson3Test {
         String textMessage = "11111111";
         int textLength = textMessage.length();
         assertTrue(textLength > 15, "It less than you need, add " + (16 - textLength) + " symbols");
-
-        {
-
-
-        }
 
     }
 
@@ -37,6 +33,27 @@ public class Lesson3Test {
 
         assertTrue(actualCookie.containsKey(expectedKey), "There is no cookie named " + expectedKey);
         assertEquals(expectedValue, actualCookie.get(expectedKey), "Value is not correct");
+
+    }
+
+
+    @Test
+    public void testActualHeader() {
+
+        String expectedHeader = "x-secret-homework-header";
+        String expectedValue = "Some secret value";
+
+        Response response = given()
+                .get("https://playground.learnqa.ru/api/homework_header")
+                .andReturn();
+
+
+        Headers headers = response.getHeaders();
+        Header header = headers.get(expectedHeader);
+
+        assertNotNull(header, "There is no header named " + expectedHeader);
+        assertEquals(expectedValue, header.getValue(), "Value is not correct");
+
 
     }
 
