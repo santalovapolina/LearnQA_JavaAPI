2de48ac2ac3bf802dee89c4ec3e4565d7d2f81e0
Polina Santalova <polina.santalova@ecm-consulting.ru>
2025-02-22 12:56:07 +0300

    some refactoring

--- src/test/java/Lesson3Test.java
+++ src/test/java/Lesson3Test.java
@@ -20,7 +20,23 @@ public class Lesson3Test {
     }
 
     @Test
-    public void testActualCookie() {
+    public void testCookieExists() {
+
+        String expectedKey = "HomeWork";
+
+        Response response = given()
+                .get("https://playground.learnqa.ru/api/homework_cookie")
+                .andReturn();
+
+        Map<String, String> actualCookie = response.cookies();
+
+        assertTrue(actualCookie.containsKey(expectedKey), "There is no cookie named " + expectedKey);
+
+    }
+
+
+    @Test
+    public void testCookieValue() {
 
         String expectedKey = "HomeWork";
         String expectedValue = "hw_value";
@@ -31,17 +47,15 @@ public class Lesson3Test {
 
         Map<String, String> actualCookie = response.cookies();
 
-        assertTrue(actualCookie.containsKey(expectedKey), "There is no cookie named " + expectedKey);
         assertEquals(expectedValue, actualCookie.get(expectedKey), "Value is not correct");
 
     }
 
 
     @Test
-    public void testActualHeader() {
+    public void testHeaderExists() {
 
         String expectedHeader = "x-secret-homework-header";
-        String expectedValue = "Some secret value";
 
         Response response = given()
                 .get("https://playground.learnqa.ru/api/homework_header")
@@ -52,10 +66,24 @@ public class Lesson3Test {
         Header header = headers.get(expectedHeader);
 
         assertNotNull(header, "There is no header named " + expectedHeader);
-        assertEquals(expectedValue, header.getValue(), "Value is not correct");
-
 
     }
 
 
+    @Test
+    public void testHeaderValue() {
+
+        String expectedHeader = "x-secret-homework-header";
+        String expectedValue = "Some secret value";
+
+        Response response = given()
+                .get("https://playground.learnqa.ru/api/homework_header")
+                .andReturn();
+
+        Headers headers = response.getHeaders();
+        Header header = headers.get(expectedHeader);
+
+        assertEquals(expectedValue, header.getValue(), "Value is not correct");
+
+    }
 }
